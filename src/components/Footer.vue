1215f36a228ab43955acac1953990e1b692f9cf5
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2018-06-03 16:12:08 +0530

    EmitOn event handling

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+     <h3> Application footer title : {{ title }}</h3>
+  </div>
+</template>
+
+<script>
+import eventBus from './EventBus.vue';
+export default {
+  name: 'Footer',
+  data(){
+    return{
+      title :"App"
+    }
+  },
+  created(){
+    eventBus.$on("changeTitle",(title)=>{this.title = title;})
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h3> Application header title : {{ title }}</h3>
+    <input type="button" v-on:click="changeTitle" value="Ok" />
+  </div>
+</template>
+
+<script>
+import eventBus from './EventBus.vue';
+export default {
+  name: 'Header',
+  data(){
+    return {
+      title : "App"
+    }
+  },
+  methods: {
+    changeTitle(){
+      this.title ="Emit-On Demo";
+      eventBus.$emit("changeTitle","Emit-On Demo");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
